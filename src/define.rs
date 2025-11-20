08e7529db94118c646fa1eab5e7fe80e76648390
8bitTD <8bittd@gmail.com>
2025-11-20 09:35:22 +0900

    アイコンの解像度を変更

--- src/define.rs
+++ src/define.rs
@@ -2,5 +2,5 @@ pub mod common{
     pub const TOOLNAME: &str = "gif_bevy";
     pub const DOCUMENT: &str = "\\Documents\\script\\Rust\\";
     pub const LIMITWIDTH: u32 = 32768;
-    pub const ICONURL: &str = "https://img.icons8.com/?size=100&id=3gTXWvZ6HMIf&format=png&color=000000";
+    pub const ICONURL: &str = "https://img.icons8.com/?size=64&id=3gTXWvZ6HMIf&format=png&color=000000";
 }
